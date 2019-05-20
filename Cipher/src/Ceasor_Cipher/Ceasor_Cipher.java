a43b07fcac46bfa337e2ce60548884c3bf361727
eyadShhadeh <50517880+eyadShhadeh@users.noreply.github.com>
2019-05-20 12:11:27 +0200

    Add files via upload

---
+++ Cipher/src/Ceasor_Cipher/Ceasor_Cipher.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ceasor_Cipher;
+
+import javax.swing.JFrame;
+
+
+
+/**
+ *
+ * @author me
+ */
+public class Ceasor_Cipher {
+
+   
+    public static void main(String[] args) {
+        CeasorCipherGUI cc = new CeasorCipherGUI();
+         cc.setSize(639, 387);
+        cc.setVisible(true);
+        cc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   
+       
+    }
+    
+}
